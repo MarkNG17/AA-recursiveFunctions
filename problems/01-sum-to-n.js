b66e99da197494697db3babd250073c5e538a2bb
MarkNG17 <mngayler@gmail.com>
2024-03-07 20:00:27 +0000

    second commit

---
+++ problems/01-sum-to-n.js
@@ -0,0 +1,40 @@
+/***********************************************************************
+Write a recursive function called `sumToN` that takes in a number and returns
+the sum of all the numbers from 0 to that number. Return null for any input
+number below 0.
+
+Examples:
+
+sumToN(5) // returns 15
+sumToN(1)  // returns 1
+sumToN(9)  // returns 45
+sumToN(-8)  // returns null
+***********************************************************************/
+
+function sumToN(num, acc = 0)
+{
+  if (num < 0)
+  {
+    return null;
+  }
+  if (num === 0)
+  {
+      return acc;
+  }
+  acc += num;
+  return sumToN(num - 1, acc);
+
+}
+
+sumToN(5) // returns 15
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try
+{
+  module.exports = sumToN;
+} catch (e)
+{
+  module.exports = null;
+}
+
+console.log(sumToN(5));
